afe7aa89f7362b7fb6f73f77e62dd2d24978e6a0
nmaju <nmajutee@gmail.com>
2025-02-26 16:06:46 +0100

    2. Utilizing the Django Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_date = models.IntegerField()
+    published_date = models.IntegerField()
     
     def __str__(self):
-        return f"{self.title} by {self.author} on {self.publication_date}"
+        return f"{self.title} by {self.author} on {self.published_date}"
