133c35e4151b3244b184ae5c607f53d3aaad1abd
nmaju <nmajutee@gmail.com>
2025-03-01 06:01:08 +0100

    0. Implementing Advanced Model Relationships in Django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def query_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name = author_name)
+        books = Book.objects.filter(author = author)
+        return books
+    except Author.DoesNotExist:
+        return f" Author {author_name} not found."
+
+def list_all_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name = library_name)
+        books = Book.objects.all()
+        return books
+    except Library.DoesNotExist:
+        return f"Library {library_name} not found."
