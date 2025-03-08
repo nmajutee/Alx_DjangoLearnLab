eb693efe2d51a1538a8b303079c6d2111250b642
nmaju <nmajutee@gmail.com>
2025-03-08 11:48:30 +0100

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .views import LibraryListView
+from .views import list_books
+from django.views.generic import ListView
 
 urlpatterns = [
-    path('relationship_app/list_books.html', views.list_book_view, name='list books'),
-    path('relationship_app/library_detail.html', LibraryListView.as_view(), name='list_books'),
-]
+    path('relationship_app/list_books.html', views.list_book_view, name='list_books'),
+    
+    path('relationship_app/library_detail.html', ListView.as_view(template_name='library_detail.html'))
+    ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,18 @@
 from relationship_app.models import Author, Book, Library, Librarian
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView
 from django.shortcuts import render
 from .models import Library
 
 def list_books(request):
     books = Book.objects.all()
     context = {'book_list':'books'}
-    return (request, render, 'relationship_app/list_books.html', context )
-
-class LibraryListView(ListView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
+    return (request, render, 'list_books.html', context )
 
 class LibraryDetailView(DetailView):
-    pass
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.objects.books.all()
