bb24c199ef4f5f713e9a8a297cb0f16170ec6935
nmaju <nmajutee@gmail.com>
2025-03-08 11:49:59 +0100

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,11 @@ from .models import Library
 def list_books(request):
     books = Book.objects.all()
     context = {'book_list':'books'}
-    return (request, render, 'list_books.html', context )
+    return (request, render, 'relationship_app/list_books.html', context )
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     
     def get_context_data(self, **kwargs):
